cfd7d8ddb8fa29bfa201f4c76ea7aa1f0a804c64
Sam Carlton <sam@sam.lc>
2022-03-19 00:34:51 -0500

    Store urls via json

--- scripts/flat-postprocessing.mjs
+++ scripts/flat-postprocessing.mjs
@@ -1,21 +1,44 @@
 // Disable ESLint console logging rule for this file
 /* eslint-disable */
 import fs from 'fs-extra'
+
 import { downloadSite } from '../src/utils/download-site.mjs'
 
 const scrapePath = './scrape'
 
 ;(async () => {
 
+  const docsUrls = await fs.readJson('./public/docs-urls.json')
+
   await fs.remove( scrapePath )
 
-  console.log(`Pulling in URLs`)
+  console.log(`Pulling in URLs`, docsUrls)
 
-  const pageList = await downloadSite({
+  const {
+    result
+  } = await downloadSite({
     path: scrapePath,
   })
 
-  console.log( 'pageList', pageList.length )
+  const htmlResourceUrls = result
+    .filter(resource => resource.type === 'html')
+    .map(resource => resource.url)
+
+
+  // Merge the docsUrls with the htmlResourceUrls, remove duplicates, and sort
+  const mergedUrls = Array.from(new Set([
+    ...docsUrls,
+    ...htmlResourceUrls,
+  ]))
+  .sort()
+
+  // Save htmlResourceUrls to json
+  await fs.writeJSON( `./public/docs-urls.json`, mergedUrls, { spaces: '\t' } )
+
+  console.log( 'htmlResourceUrls', htmlResourceUrls )
+  // console.log( 'result type', result[0].type )
+
+  console.log( 'result', result.length )
 
   console.log('Pull complete.')
 
