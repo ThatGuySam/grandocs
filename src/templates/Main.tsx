4b1c5155829641b111c236d937fcf9e6f71cc6c9
Sam Carlton <sam@sam.lc>
2022-03-18 21:46:03 -0500

    Add MA disclaimer

--- src/templates/Main.tsx
+++ src/templates/Main.tsx
@@ -46,17 +46,8 @@ const Main = (props: IMainProps) => (
       <div className="py-5 text-xl content">{props.children}</div>
 
       <div className="py-8 text-sm text-center border-t border-gray-300">
-        © Copyright {new Date().getFullYear()} {AppConfig.title}. Powered with{' '}
-        <span role="img" aria-label="Love">
-          ♥
-        </span>{' '}
-        by <a href="https://creativedesignsguru.com">CreativeDesignsGuru</a>
-        {/*
-         * PLEASE READ THIS SECTION
-         * We'll really appreciate if you could have a link to our website
-         * The link doesn't need to appear on every pages, one link on one page is enough.
-         * Thank you for your support it'll mean a lot for us.
-         */}
+        This site is not associated with or endorsed by MA Lighting
+        International GmbH
       </div>
     </div>
   </div>
