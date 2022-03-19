28ad9499e0cd983de46fa94409947522e5377039
Sam Carlton <sam@sam.lc>
2022-03-18 20:40:00 -0500

    Switch to a next+tailwind boilerplate

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,10 @@
+// Please do not use the array form (like ['tailwindcss', 'postcss-preset-env'])
+// it will create an unexpected error: Invalid PostCSS Plugin found: [0]
+
 module.exports = {
-  plugins: [
-    'postcss-import',
-    'tailwindcss/nesting',
-    'tailwindcss',
-    'postcss-focus-visible',
-    'autoprefixer',
-  ],
-}
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}),
+  },
+};
